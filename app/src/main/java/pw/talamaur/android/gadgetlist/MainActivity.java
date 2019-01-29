de8c367dcf98f0f97a89fdee19276bf97ca70fa4
alebra42 <4082231+alebra42@users.noreply.github.com>
2019-01-29 16:36:36 +0700

    Added listener for list

--- app/src/main/java/pw/talamaur/android/gadgetlist/MainActivity.java
+++ app/src/main/java/pw/talamaur/android/gadgetlist/MainActivity.java
@@ -3,11 +3,17 @@ package pw.talamaur.android.gadgetlist;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewGadgets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageViewLogo = findViewById(R.id.imageViewLogo);
         Picasso.get().load(R.drawable.logo).fit().centerInside().into(imageViewLogo);
+
+        listViewGadgets = findViewById(R.id.listViewGadgets);
+        listViewGadgets.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        //TODO
+                        break;
+
+                    default:
+                        Toast.makeText(MainActivity.this, R.string.unsupported_category,
+                                Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void hideActionBar() {
